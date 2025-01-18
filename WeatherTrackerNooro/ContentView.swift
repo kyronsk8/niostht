717b03b813cd583ffb0fb99703811c1c6e3336cd
kyronsk8 <stupidkyron@gmail.com>
2025-01-17 20:22:03 -0500

    Initial Commit

---
+++ WeatherTrackerNooro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherTrackerNooro
+//
+//  Created by Kyron Weaver on 1/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherTrackerNooro/WeatherTrackerNooroApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerNooroApp.swift
+//  WeatherTrackerNooro
+//
+//  Created by Kyron Weaver on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerNooroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
